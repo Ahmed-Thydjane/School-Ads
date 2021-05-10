2c4769d8535e0a30b195cdcd5d604aa6b818a875
NARChristian <christiann1v5@gmail.com>
2021-05-10 19:03:44 +0200

    new version de la base de bonnée

--- school-ads.sql
+++ school-ads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 30 avr. 2021 à 14:53
+-- Généré le : lun. 10 mai 2021 à 17:01
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -64,19 +64,26 @@ CREATE TABLE IF NOT EXISTS `annonce` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilsateur`
+-- Structure de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilsateur`;
-CREATE TABLE IF NOT EXISTS `utilsateur` (
-  `iduser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `prenom` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `nom` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `role` int(1) NOT NULL COMMENT '0 pour etudiant\r\n1 pour professeur',
+DROP TABLE IF EXISTS `utilisateur`;
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(256) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `prenom` varchar(256) NOT NULL,
+  `role` int(2) NOT NULL,
   PRIMARY KEY (`iduser`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`iduser`, `nom`, `email`, `password`, `prenom`, `role`) VALUES
+(1, 'OUEDRAOGO', 'christiann1v5@gmail.com', 'root', 'Narelba', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
