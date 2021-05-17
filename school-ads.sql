64d981e124449ffaa7f11e140d35db24fb8d3092
Ahmed-Thydjane <cheikh-ahmed-t.GNANKENE@etu.univ-amu.fr>
2021-05-18 01:45:13 +0200

    modification de la bd

--- school-ads.sql
+++ school-ads.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 10 mai 2021 à 17:01
+-- Généré le : lun. 17 mai 2021 à 23:43
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -57,6 +57,12 @@ CREATE TABLE IF NOT EXISTS `annonce` (
   `duree` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
   `vue` int(11) NOT NULL,
   `candidature` int(11) NOT NULL,
+  `duree_max` varchar(30) CHARACTER SET utf32 COLLATE utf32_unicode_ci DEFAULT NULL,
+  `duree_min` varchar(30) CHARACTER SET utf32 COLLATE utf32_unicode_ci DEFAULT NULL,
+  `tel_responsable` varchar(40) CHARACTER SET utf32 COLLATE utf32_unicode_ci DEFAULT NULL,
+  `email_responsable` varchar(100) CHARACTER SET utf32 COLLATE utf32_unicode_ci DEFAULT NULL,
+  `postuler` tinyint(1) DEFAULT NULL,
+  `status` int(1) DEFAULT NULL,
   PRIMARY KEY (`idannonce`),
   KEY `iduser` (`iduser`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -69,21 +75,14 @@ CREATE TABLE IF NOT EXISTS `annonce` (
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `iduser` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(256) NOT NULL,
-  `email` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  `prenom` varchar(256) NOT NULL,
-  `role` int(2) NOT NULL,
+  `iduser` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `nom` varchar(50) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `role` varchar(20) COLLATE utf32_unicode_ci NOT NULL,
   PRIMARY KEY (`iduser`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `utilisateur`
---
-
-INSERT INTO `utilisateur` (`iduser`, `nom`, `email`, `password`, `prenom`, `role`) VALUES
-(1, 'OUEDRAOGO', 'christiann1v5@gmail.com', 'root', 'Narelba', 0);
+) ENGINE=MyISAM DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
